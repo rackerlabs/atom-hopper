1e297c37a19a108e8c8328cab53a59ff9b331e52
Lisa Clark <lisa.clark@rackspace.com>
2011-05-24 21:02:52 -0500

    incremental check in of next/prev marker fix

--- hopper/src/main/java/net/jps/atom/hopper/abdera/filter/FeedPagingProcessor.java
+++ hopper/src/main/java/net/jps/atom/hopper/abdera/filter/FeedPagingProcessor.java
@@ -12,24 +12,46 @@ import org.apache.commons.lang.StringUtils;
  */
 public class FeedPagingProcessor implements AdapterResponseProcessor<Feed> {
 
+    private static final String NEXT_LINK = "next";
+    private static final String PREV_LINK = "prev";
+    private static final String CURRENT_LINK = "current";
+
+
     @Override
     public void process(RequestContext rc, AdapterResponse<Feed> adapterResponse) {
         final Feed f = adapterResponse.getBody();
+        final String self = StringUtils.join(new String[]{rc.getBaseUri().toString(), rc.getTargetPath()});
         final int numEntries = f.getEntries().size();
 
-        if (numEntries > 0) {
-            final Entry first = f.getEntries().get(0), last = f.getEntries().get(numEntries - 1);
+        if (numEntries == 0) {
+            return;
+        }
 
-            if (first.getId() != null && last.getId() != null) {
-                final String nextMarker = last.getId().toString(), previousMarker = first.getId().toString();
+        if (linkNotSet(f, CURRENT_LINK)) {
+            f.addLink(self, CURRENT_LINK);
+        }
 
-                final String self = StringUtils.join(new String[]{rc.getBaseUri().toString(), rc.getTargetPath()});
+        if (linkNotSet(f, NEXT_LINK)) {
+            final Entry last = f.getEntries().get(numEntries - 1);
 
-                // Add markers
-                f.addLink(self, "current");
-                f.addLink(StringUtils.join(new String[]{self, "?marker=", nextMarker}), "next");
-                f.addLink(StringUtils.join(new String[]{self, "?marker=", previousMarker}), "prev");
+            if (last.getId() != null) {
+                final String nextMarker = last.getId().toString();
+                f.addLink(StringUtils.join(new String[]{self, "?marker=", nextMarker}), NEXT_LINK);
             }
         }
+
+        if (linkNotSet(f, PREV_LINK)) {
+            final Entry first = f.getEntries().get(0);
+            if (first.getId() != null) {
+                final String previousMarker = first.getId().toString();
+                f.addLink(StringUtils.join(new String[]{self, "?marker=", previousMarker}), PREV_LINK);
+            }
+        }
+
+
+    }
+
+    private boolean linkNotSet(Feed feed, String link) {
+        return (feed.getLinks(link).size() == 0);
     }
 }
