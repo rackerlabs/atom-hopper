cd45f64be0b4d4fb5a68db28f1c2347590894263
Chad Lung <chad.lung@gmail.com>
2011-08-29 11:33:09 -0500

    - initial changes to the tests (work in progress)

--- test-suite/src/test/java/org/atomhopper/PostAndGetMultipleEntriesIntegrationTest.java
+++ test-suite/src/test/java/org/atomhopper/PostAndGetMultipleEntriesIntegrationTest.java
@@ -10,10 +10,9 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
-
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
 
 @RunWith(Enclosed.class)
 public class PostAndGetMultipleEntriesIntegrationTest extends JettyIntegrationTestHarness {
@@ -23,21 +22,21 @@ public class PostAndGetMultipleEntriesIntegrationTest extends JettyIntegrationTe
     private static final String urlAndPort = "http://localhost:" + getPort();
 
     public static GetMethod getFeedMethod() {
-        return new GetMethod(urlAndPort + "/namespace/feed/");
+        return new GetMethod(urlAndPort + "/namespace1/feed1/");
     }
     
     public static GetMethod getMarkerFeedMethod(String markerId) {
-        return new GetMethod(urlAndPort + "/namespace/feed/?marker=" + markerId + "&limit=5");
+        return new GetMethod(urlAndPort + "/namespace1/feed1/?marker=" + markerId + "&limit=5");
     }    
 
     public static GetMethod getEntryMethod(String entryId) {
-        return new GetMethod(urlAndPort + "/namespace/feed/entries/" + entryId);
+        return new GetMethod(urlAndPort + "/namespace1/feed1/entries/" + entryId);
     }
 
-    public static PostMethod newPostEntryMethod(String entryId, String content) {
-        final PostMethod post = new PostMethod(urlAndPort + "/namespace/feed/");
+    public static PostMethod newPostEntryMethod(String content) {
+        final PostMethod post = new PostMethod(urlAndPort + "/namespace1/feed1/");
         post.addRequestHeader(new Header("content-type", "application/atom+xml"));
-        post.setRequestBody("<?xml version=\"1.0\" ?><entry xmlns=\"http://www.w3.org/2005/Atom\"><id>" + entryId + "</id><author><name>Chad</name></author><content>" + content + "</content></entry>");
+        post.setRequestBody("<?xml version=\"1.0\" ?><entry xmlns=\"http://www.w3.org/2005/Atom\"><author><name>Chad</name></author><content>" + content + "</content></entry>");
 
         return post;
     }
@@ -48,17 +47,16 @@ public class PostAndGetMultipleEntriesIntegrationTest extends JettyIntegrationTe
         public void shouldReturnPartialFeed() throws Exception {
             // Create 15 new entries
             for(int i = 200; i < 216; i++) {
-                final HttpMethod postMethod = newPostEntryMethod(Integer.toString(i), "<blah><a1>a1</a1><b1>b1</b1></blah>");
+                final HttpMethod postMethod = newPostEntryMethod("<blah><a1>a1</a1><b1>" + Integer.toString(i) + "</b1></blah>");
                 assertEquals("Creating a new entry should return a 201", HttpStatus.SC_CREATED, httpClient.executeMethod(postMethod));
             }
             
             final HttpMethod getFeedMethod = getFeedMethod();
             assertEquals("Getting a feed should return a 200", HttpStatus.SC_OK, httpClient.executeMethod(getFeedMethod));
-            System.out.println(new String(getFeedMethod.getResponseBody()));
+            //System.out.println(new String(getFeedMethod.getResponseBody()));
             
-            final HttpMethod getMarkerFeedMethod = getMarkerFeedMethod("205");
-            assertEquals("Getting a recently added entry should return a 200", HttpStatus.SC_OK, httpClient.executeMethod(getMarkerFeedMethod));
-            System.out.println(new String(getFeedMethod.getResponseBody()));
+            assertEquals("Getting the new feed should show that <b1>215</b1> was added", HttpStatus.SC_OK, httpClient.executeMethod(getFeedMethod));
+            assertTrue(new String(getFeedMethod.getResponseBody()).contains("<b1>215</b1>"));
         }
     }    
 }
