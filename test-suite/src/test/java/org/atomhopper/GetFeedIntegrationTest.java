7bbea6ecfa2bf52ebdec952242f732312de931d3
Chad Lung <chad.lung@gmail.com>
2012-02-18 09:55:29 -0600

    - added test to ensure the oldest-entry link is showing up

--- test-suite/src/test/java/org/atomhopper/GetFeedIntegrationTest.java
+++ test-suite/src/test/java/org/atomhopper/GetFeedIntegrationTest.java
@@ -68,10 +68,12 @@ public class GetFeedIntegrationTest extends JettyIntegrationTestHarness {
             Document doc = xml.toDOM(getFeedMethod.getResponseBodyAsString());
             XPath xPath = XPathFactory.newInstance().newXPath();
             String linkUrl = xPath.evaluate("/feed/link[@rel='next']/@href", doc);
+            String linkUrlOldestEntry = xPath.evaluate("/feed/link[@rel='oldest-entry']/@href", doc);
 
             assertNotNull("The returned XML should not be null", doc);
             xml.assertHasValue(doc, "/feed/link[@rel='current']/@href", urlAndPort + "/namespace/feed");
             xml.assertHasValue(doc, "/feed/link[@rel='next']/@href", linkUrl);
+            xml.assertHasValue(doc, "/feed/link[@rel='oldest-entry']/@href", linkUrlOldestEntry);
         }
 
         @Test
