5f918c9a6084b8278fdd857525fca4f5ed4d30a2
Seth Brayman <seth.brayman@rackspace.com>
2012-03-14 16:02:45 -0500

    Johns tests from the brown bag with some changes added to the app context files.

--- adapters/hibernate/src/test/java/org/atomhopper/hibernate/HibernateFeedRepositoryTest.java
+++ adapters/hibernate/src/test/java/org/atomhopper/hibernate/HibernateFeedRepositoryTest.java
@@ -1,16 +1,18 @@
 package org.atomhopper.hibernate;
 
 import org.atomhopper.dbal.AtomDatabaseException;
+import org.atomhopper.hibernate.actions.SimpleSessionAction;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-import org.atomhopper.hibernate.actions.SimpleSessionAction;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * User: sbrayman Date: 2/27/12
@@ -18,64 +20,64 @@ import static org.mockito.Mockito.*;
 @RunWith(Enclosed.class)
 public class HibernateFeedRepositoryTest {
 
-   public static final SimpleSessionAction NOP_SIMPLE_ACTION = new SimpleSessionAction() {
+    public static final SimpleSessionAction NOP_SIMPLE_ACTION = new SimpleSessionAction() {
 
-      @Override
-      public void perform(Session liveSession) {
-      }
-   };
+        @Override
+        public void perform(Session liveSession) {
+        }
+    };
 
-   public static class WhenPerformingSimpleAction {
+    public static class WhenPerformingSimpleAction {
 
-      protected HibernateFeedRepository feedRepository;
-      protected Transaction mockedTransaction;
-      protected Session mockedSession;
+        protected HibernateFeedRepository feedRepository;
+        protected Transaction mockedTransaction;
+        protected Session mockedSession;
 
-      @Before
-      public void setup() throws Exception {
-         mockedTransaction = mock(Transaction.class);
+        @Before
+        public void setup() throws Exception {
+            mockedTransaction = mock(Transaction.class);
 
-         mockedSession = mock(Session.class);
-         when(mockedSession.beginTransaction()).thenReturn(mockedTransaction);
+            mockedSession = mock(Session.class);
+            when(mockedSession.beginTransaction()).thenReturn(mockedTransaction);
 
-         final SessionManager sessionManager = mock(SessionManager.class);
-         when(sessionManager.getSession()).thenReturn(mockedSession);
+            final SessionManager sessionManager = mock(SessionManager.class);
+            when(sessionManager.getSession()).thenReturn(mockedSession);
 
-         feedRepository = new HibernateFeedRepository(sessionManager);
-      }
+            feedRepository = new HibernateFeedRepository(sessionManager);
+        }
 
-      @Test(expected = AtomDatabaseException.class)
-      public void shouldHandleExceptionsThrownFromAction() {
-         final SimpleSessionAction myAction = new SimpleSessionAction() {
+        @Test(expected = AtomDatabaseException.class)
+        public void shouldHandleExceptionsThrownFromAction() {
+            final SimpleSessionAction myAction = new SimpleSessionAction() {
 
-            @Override
-            public void perform(Session liveSession) {
-               throw new RuntimeException("Failure occured.");
+                @Override
+                public void perform(Session liveSession) {
+                    throw new RuntimeException("Failure occured.");
+                }
+            };
+
+            try {
+                feedRepository.performSimpleAction(myAction);
+            } finally {
+                verify(mockedTransaction, times(1)).rollback();
+                verify(mockedSession, times(1)).close();
             }
-         };
+        }
 
-         try {
-            feedRepository.performSimpleAction(myAction);
-         } finally {
-            verify(mockedTransaction, times(1)).rollback();
+        @Test(expected = AtomDatabaseException.class)
+        public void shouldHandleExceptionsInStartingTransactions() {
+            when(mockedSession.beginTransaction()).thenThrow(new RuntimeException("Failure in starting transaction"));
+
+            feedRepository.performSimpleAction(NOP_SIMPLE_ACTION);
+        }
+
+        @Test
+        public void shouldPerformSimpleAction() {
+            feedRepository.performSimpleAction(NOP_SIMPLE_ACTION);
+
+            verify(mockedSession, times(1)).beginTransaction();
+            verify(mockedTransaction, times(1)).commit();
             verify(mockedSession, times(1)).close();
-         }
-      }
-
-      @Test(expected = AtomDatabaseException.class)
-      public void shouldHandleExceptionsInStartingTransactions() {
-         when(mockedSession.beginTransaction()).thenThrow(new RuntimeException("Failure in starting transaction"));
-
-         feedRepository.performSimpleAction(NOP_SIMPLE_ACTION);
-      }
-
-      @Test
-      public void shouldPerformSimpleAction() {
-         feedRepository.performSimpleAction(NOP_SIMPLE_ACTION);
-
-         verify(mockedSession, times(1)).beginTransaction();
-         verify(mockedTransaction, times(1)).commit();
-         verify(mockedSession, times(1)).close();
-      }
-   }
+        }
+    }
 }
