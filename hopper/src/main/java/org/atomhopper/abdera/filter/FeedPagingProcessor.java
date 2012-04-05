56f7bb526c9cd07b02faea13776bcd31b3c16357
Seth Brayman <seth.brayman@rackspace.com>
2012-04-05 13:35:33 -0500

    Committing work for the ampersand bug to another branch.

--- hopper/src/main/java/org/atomhopper/abdera/filter/FeedPagingProcessor.java
+++ hopper/src/main/java/org/atomhopper/abdera/filter/FeedPagingProcessor.java
@@ -1,18 +1,17 @@
 package org.atomhopper.abdera.filter;
 
+import org.apache.abdera.i18n.text.UrlEncoding;
 import org.apache.abdera.model.Feed;
 import org.apache.abdera.protocol.server.RequestContext;
 import org.apache.commons.lang.StringUtils;
 import org.atomhopper.response.AdapterResponse;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static java.net.URLEncoder.encode;
-import java.util.*;
+import java.util.TimeZone;
 
 /**
  *
@@ -48,17 +47,18 @@ public class FeedPagingProcessor implements AdapterResponseInterceptor<Feed> {
         if (linkNotSet(f, CURRENT_LINK)) {
             f.addLink(StringUtils.join(new String[]{self, mapToParameters(parameters)}), CURRENT_LINK);
         }
+
         // Add self link (same as current link)
         if (linkNotSet(f, SELF_LINK)) {
             f.addLink(StringUtils.join(new String[]{self, mapToParameters(parameters)}), SELF_LINK);
-        }        
+        }
 
         // If the feed source hasn't already defined this link
         if (linkNotSet(f, NEXT_LINK)) {
             String id = f.getEntries().get(f.getEntries().size() - 1).getId().toString();
-            
+
             if (parameters.containsKey(DIRECTION)) {
-                if(parameters.get(DIRECTION).get(0).equalsIgnoreCase("forward")) {
+                if (parameters.get(DIRECTION).get(0).equalsIgnoreCase("forward")) {
                     id = f.getEntries().get(0).getId().toString();
                 }
             }
@@ -78,9 +78,9 @@ public class FeedPagingProcessor implements AdapterResponseInterceptor<Feed> {
         for (String parameter : rc.getParameterNames()) {
             ArrayList<String> values = new ArrayList<String>();
             for (String value : rc.getParameters(parameter)) {
-                values.add(value);
+                values.add(value.replace("amp;", ""));
             }
-            parameters.put(parameter.toLowerCase(), values);
+            parameters.put(parameter.toLowerCase().replace("amp;", ""), values);
         }
 
         return parameters;
@@ -89,22 +89,50 @@ public class FeedPagingProcessor implements AdapterResponseInterceptor<Feed> {
     public static String mapToParameters(Map<String, List<String>> parameters) {
         try {
             List<String> result = new ArrayList<String>();
+            String queryString = "";
+
+            // make string with params
+            // unencode string
+            //do everything else
+            //re-encode and pass back
 
             // Combine the keys into a key=value list
             for (String key : parameters.keySet()) {
                 //The key isn't unique, and we might end up with an array of multiple parameters
                 for (String value : parameters.get(key)) {
-                    result.add(encode(key, "UTF-8") + '=' + encode(value, "UTF-8"));
+                    result.add(key + '=' + value);
                 }
             }
 
-            if (result.isEmpty()) {
+            queryString = StringUtils.join(result.toArray(), "&");
+
+            //queryString = encode(queryString, "UTF-8");
+
+            String s = UrlEncoding.decode(queryString);
+
+            if (queryString == null || queryString.isEmpty()) {
                 return "";
             }
 
+/*
+            for (int i = 0; i < result.size(); i++) {
+                queryString += result.get(i);
+                if (i + 1 < result.size() && !result.get(i).contains("&amp;") && !result.get(i).contains("amp%3B")) {
+                    queryString += "&";
+                }
+            }
+*/
+
+/*
+            String s = StringUtils.join(new String[]{"?", StringUtils.join(result.toArray(), "&")});
+
             // Join the list into a string separated by '&' and prefix with '?'
-            return StringUtils.join(new String[]{"?", StringUtils.join(result.toArray(), "&")});
-        } catch (UnsupportedEncodingException e) {
+            return s;
+*/
+
+            return "?" + queryString;
+
+        } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
     }
