8af40e656b4360f9bac63d121a8a9bcc451e11f0
Seth <seth.brayman@rackspace.com>
2012-08-13 15:04:48 -0500

    Added config loader.

---
+++ hopper/src/main/java/org/atomhopper/ExternalConfigLoaderContextListener.java
@@ -0,0 +1,33 @@
+package org.atomhopper;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple utility listener to load certain properties before Spring Starts up.
+ */
+public class ExternalConfigLoaderContextListener implements ServletContextListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExternalConfigLoaderContextListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        String configLocation = sce.getServletContext().getInitParameter("CONFIGDIR");
+        if (configLocation == null) {
+            configLocation = "/etc/atomhopper/";
+        }
+
+        try {
+            new LogBackConfigLoader(configLocation + "logback.xml");
+        } catch (Exception e) {
+            logger.error("Unable to read config file", e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
