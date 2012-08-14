3c9b274006e35ced2ae17e74302d4da65258f317
Seth <seth.brayman@rackspace.com>
2012-08-14 14:21:23 -0500

    Some refactoring.  Adding logback to jetty.

--- hopper/src/main/java/org/atomhopper/LogBackConfigLoader.java
+++ hopper/src/main/java/org/atomhopper/LogBackConfigLoader.java
@@ -13,7 +13,7 @@ import ch.qos.logback.core.joran.spi.JoranException;
 /**
  * Simple Utility class for loading an external config file for logback
  *
- * This code is from https://bowerstudios.com/node/896
+ * This code is modified from https://bowerstudios.com/node/896
  */
 public class LogBackConfigLoader {
 
@@ -25,21 +25,17 @@ public class LogBackConfigLoader {
         File externalConfigFile = new File(externalConfigFileLocation);
         if (!externalConfigFile.exists()) {
             throw new IOException("Logback External Config File Parameter does not reference a file that exists");
+        } else if (!externalConfigFile.isFile()) {
+            throw new IOException("Logback External Config File Parameter exists, but does not reference a file");
+        } else if (!externalConfigFile.canRead()) {
+            throw new IOException("Logback External Config File exists and is a file, but cannot be read.");
         } else {
-            if (!externalConfigFile.isFile()) {
-                throw new IOException("Logback External Config File Parameter exists, but does not reference a file");
-            } else {
-                if (!externalConfigFile.canRead()) {
-                    throw new IOException("Logback External Config File exists and is a file, but cannot be read.");
-                } else {
-                    JoranConfigurator configurator = new JoranConfigurator();
-                    configurator.setContext(lc);
-                    lc.reset();
-                    configurator.doConfigure(externalConfigFileLocation);
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(lc);
+            lc.reset();
+            configurator.doConfigure(externalConfigFileLocation);
 
-                    logger.info("Configured Logback with config file from: " + externalConfigFileLocation);
-                }
-            }
+            logger.info("Configured Logback with config file from: " + externalConfigFileLocation);
         }
     }
 }
