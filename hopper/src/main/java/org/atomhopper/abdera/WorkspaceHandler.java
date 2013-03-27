540f7f3d9776759a22e6c74426c864dc6bfeb7e2
Omanand Jha Vatsa <vatsaomanand@gmail.com>
2013-03-27 12:58:24 +0530

    Adding support for regex based feed references.
    For supporting regular expression based dynamic feed references,
    enhanced answering adapter search to match regular expression instead
    of direct key/values search in collectionAdapterMap.

--- hopper/src/main/java/org/atomhopper/abdera/WorkspaceHandler.java
+++ hopper/src/main/java/org/atomhopper/abdera/WorkspaceHandler.java
@@ -5,13 +5,14 @@ import org.apache.abdera.parser.stax.FOMWorkspace;
 import org.apache.abdera.protocol.server.CollectionInfo;
 import org.apache.abdera.protocol.server.RequestContext;
 import org.apache.abdera.protocol.server.WorkspaceInfo;
-import org.apache.commons.lang.StringUtils;
 import org.atomhopper.config.v1_0.WorkspaceConfiguration;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class WorkspaceHandler implements WorkspaceInfo {
 
@@ -29,11 +30,21 @@ public class WorkspaceHandler implements WorkspaceInfo {
 
     public TargetAwareAbstractCollectionAdapter getAnsweringAdapter(RequestContext rc) {
         final String feedSpec = rc.getTarget().getParameter(TargetResolverField.FEED.toString());
-
-        return !StringUtils.isBlank(feedSpec) ? collectionAdapterMap.get(new StringBuilder().append("/")
-                .append(rc.getTarget().getParameter(TargetResolverField.WORKSPACE.toString()))
-                .append("/")
-                .append(feedSpec).toString()) : null;
+        final String adapterKey = new StringBuilder().append("/")
+                 .append(rc.getTarget().getParameter(TargetResolverField.WORKSPACE.toString()))
+                 .append("/")
+                 .append(feedSpec).toString();
+        
+        TargetAwareAbstractCollectionAdapter collectionAdapter = null;
+        Set<Entry<String, TargetAwareAbstractCollectionAdapter>> adapterEntries = collectionAdapterMap.entrySet();
+        
+        for(Entry<String, TargetAwareAbstractCollectionAdapter> adapterEntry : adapterEntries){
+                if(adapterKey.matches(adapterEntry.getKey())){
+                        collectionAdapter = adapterEntry.getValue();
+                        break;
+                }
+        }
+        return collectionAdapter;
     }
 
     @Override
