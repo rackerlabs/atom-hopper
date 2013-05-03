f8bc2ba067d9737982d64f36735c3a8f29fc31da
mattkovacs <matt.kovacs@rackspace.com>
2013-05-03 12:20:10 -0500

    adding back reference to enableRegexFeeds for test

--- hopper/src/main/java/org/atomhopper/abdera/WorkspaceHandler.java
+++ hopper/src/main/java/org/atomhopper/abdera/WorkspaceHandler.java
@@ -40,7 +40,7 @@ public class WorkspaceHandler implements WorkspaceInfo {
                                 .append("/")
                                 .append(feedSpec).toString();
 
-                if(false) {
+                if(myConfig.isEnableRegexFeeds()) {
                         Set<Entry<String, TargetAwareAbstractCollectionAdapter>> adapterEntries = collectionAdapterMap.entrySet();
                         for(Entry<String, TargetAwareAbstractCollectionAdapter> adapterEntry : adapterEntries) {
                                 if(adapterKey.matches(adapterEntry.getKey())) {
